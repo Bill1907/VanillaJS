2513d9a8b196c81bbcd09ff83dd320371b9314a4
Bill1907 <a31829660@gamil.com>
2022-12-16 23:24:55 +0900

    삭제 이벤트 추가

--- WebComponent/src/components/Items.js
+++ WebComponent/src/components/Items.js
@@ -8,15 +8,32 @@ class Items extends Component {
     const { items } = this.$state;
     return `
             <ul>
-              ${items.map((item) => `<li>${item}</li>`).join("")}
-            </ul>
-            <button>추가</button>
+              ${items
+                .map(
+                  (item, key) => `
+                <li>
+                  ${item}
+                  <button class="delete-btn" data-index="${key}">삭제</button>
+                </li>
+                `
+                )
+                .join("")}
+            </ul> 
+            <button class="add-btn">추가</button>
         `;
   }
   setEvent() {
-    this.$target.querySelector("button").addEventListener("click", () => {
+    this.$target.querySelector(".add-btn").addEventListener("click", () => {
       const { items } = this.$state;
       this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+
+    this.$target.querySelectorAll(".delete-btn").forEach((deleteBtn) => {
+      deleteBtn.addEventListener("click", ({ target }) => {
+        const items = [...this.$state.items];
+        items.splice(target.dataset.index, 1);
+        this.setState({ items });
+      });
     });
   }
 }
